0858160c1fdfe29e688d94d0a6360cd7f08063b8
d.brusov <dabrusov@gmail.com>
2022-10-25 17:17:32 +0300

    Добавлена страница станций

--- stations/models.py
+++ stations/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+
+# Create your models here.
+
 class Station(models.Model):
-    name = models.CharField(max_length=100)
-    address = models.TextField(max_length=300, null=True, blank=True)
+    id = models.IntegerField('Ид.', primary_key=True)
+    name = models.CharField('Наименование станции', max_length=100)
+    short_name = models.CharField('Короткое наименование станции', max_length=10, default='-')
+    company = models.CharField('Генерирующая компания', max_length=50, null=True, blank=True)
+    address = models.CharField('Адрес нахождения', max_length=100, null=True, blank=True)
+    image = models.ImageField('Изображение', blank=True, upload_to='')
 
     def __str__(self):
         return self.name
-
-# Create your models here.
--- stations/views.py
+++ stations/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from .models import Station
 
 def stations_overview(request):
-    station_list = Station.objects.all()
-    data = {'stations' : station_list}
+    company_list = [x[0] for x in set(list(Station.objects.order_by('id').values_list('company')))]
+    companies = []
+    for company in sorted(company_list):
+        stations = []
+        station_list = Station.objects.filter(company=company)
+        company_data = {
+            'name' : company,
+            'station_list' : station_list
+        }
+        companies.append(company_data)
+    data = {
+        'companies' : companies,
+    }
     return render(request, 'stations/overview.html', data)
 
 # Create your views here.
